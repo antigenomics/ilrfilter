77ae5d1c242be3249ff01bffd6451d653e3f28ff
mikessh <mikhail.shugay@gmail.com>
2019-10-28 13:16:41 +0000

    2prev

--- example/test.sh
+++ example/test.sh
@@ -1,2 +1,2 @@
-cd .. && mvn clean install && cd test/
-java -jar ../target/ilrfilter-*.jar hash -S hsa -I ../src/test/resources/reads_R1.fastq ../src/test/resources/reads_R2.fastq -O ./out
+cd .. && mvn clean install && cd example/
+java -jar ../target/ilrfilter-*.jar hash -S hsa -I ../src/test/resources/reads_R1.fastq ../src/test/resources/reads_R2.fastq -O ./out_test
